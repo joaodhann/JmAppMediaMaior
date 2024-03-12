693830bc613aa8bdd44b2170285f6be0b3b8a8c2
Usuario <Usuario@CPD02-M05>
2024-03-12 11:54:49 -0300

    Adicionar arquivos de projeto.

---
+++ JmAppMédiaMaiorcomE/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JmAppMédiaMaiorcomE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual sua média?");
+            int media = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual sua presença?");
+            int porcent = int.Parse(Console.ReadLine());
+
+            if (media >= 7 && porcent >= 75)
+            {
+                Console.WriteLine("Aprovado");
+            }
+
+            else if (media < 7 && media >= 4 && porcent >= 75 || media >= 7 && porcent < 75)
+            {
+                Console.WriteLine("Recuperação");
+            }
+
+            else 
+            {
+                Console.WriteLine("Retido");
+            }
+        
+            Console.ReadKey();
+        }
+
+
+    }
+}
